47174a712c105aa489e4d8ab5a69ced8993b585c
Isaac McGaha Miller <isaacmcgahamiller@gmail.com>
2017-03-29 12:15:32 -0600

    organized file structure

--- knexfile.js
+++ knexfile.js
@@ -4,9 +4,21 @@ module.exports = {
   development: {
     client: 'postgresql',
     connection: `postgres://localhost:5432/${databaseName}`,
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds'
+    }
   },
   test: {
     client: 'postgresql',
     connection: `postgres://localhost:5432/${databaseName}_test`,
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds'
+    }
   }
 };
