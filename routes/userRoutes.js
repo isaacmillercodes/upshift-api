150f7133336876033e8c7261d8fbdd89133b78a6
Isaac McGaha Miller <isaacmcgahamiller@gmail.com>
2017-03-29 14:37:59 -0600

    added middleware to protect future routes

---
+++ routes/userRoutes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const db = require('../db/queries');
+// const bcrypt = require('bcryptjs');
+// const salt = bcrypt.genSaltSync(10);
+// const jwt = require('jsonwebtoken');
+
+router.get('/', (req, res) => {
+  db.list('users')
+  .then(users => {
+    res.json(users);
+  });
+});
+
+module.exports = router;
