776283caec1ac9859a3bbc8eaa0bbbf38cdfec39
Isaac McGaha Miller <isaacmcgahamiller@gmail.com>
2017-03-29 14:57:42 -0600

    added update and delete for user, will likely need adjusting to account for foreign keys in other tables

--- db/queries.js
+++ db/queries.js
@@ -3,7 +3,7 @@ const knex = require('./knex');
 module.exports = {
   create: (table, object) => knex(table).insert(object).returning('*'),
   read: (table, id) => knex(table).where('id', id),
-  update: (table, id, object) => knex(table).where('id', id).update(object, 'id'),
+  update: (table, id, object) => knex(table).where('id', id).update(object, '*'),
   destroy: (table, id) => knex(table).where('id', id).del().returning('id'),
   list: table => knex(table)
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const db = require('../db/queries');
-// const bcrypt = require('bcryptjs');
-// const salt = bcrypt.genSaltSync(10);
+const bcrypt = require('bcryptjs');
+const salt = bcrypt.genSaltSync(10);
 // const jwt = require('jsonwebtoken');
 
 router.get('/', (req, res) => {
@@ -15,6 +15,47 @@ router.get('/:id', (req, res) => {
   db.read('users', req.params.id)
   .then(users => {
     res.json(users);
+  });
+});
+
+router.put('/:id', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const hash = bcrypt.hashSync(password, salt);
+
+  const updatedUser = {
+    email: email,
+    password: hash
+  };
+
+  db.update('users', req.params.id, updatedUser)
+  .then(user => {
+    res.json({
+      message: 'Successfully updated user!',
+      user: user[0]
+    });
+  })
+  .catch((err) => {
+    res.status(400).send({
+      status: 'Failed to update user',
+      message: err
+    });
+  });
+
+});
+
+router.delete('/:id', (req, res) => {
+  db.destroy('users', req.params.id)
+  .then(deletedId => {
+    res.json({
+      message: `Successfully deleted user with id ${deletedId}`
+    });
+  })
+  .catch((err) => {
+    res.status(400).send({
+      status: 'Failed to delete user',
+      message: err
+    });
   });
 });
 
