ad44e1cd5e215a188fa665b6ee7a23ac61ba3dac
Isaac McGaha Miller <isaacmcgahamiller@gmail.com>
2017-03-31 15:31:44 -0600

    adjusting votes schema as well

--- db/migrations/20170327112354_shifts.js
+++ db/migrations/20170327112354_shifts.js
@@ -1,8 +1,6 @@
 exports.up = knex => {
   return knex.schema.createTable('shifts', table => {
     table.increments();
-    table.integer('schedule_id').references('schedules.id');
-    table.date('date');
     table.time('start_time');
     table.time('end_time');
     table.string('name');
