f01b4114eaa347b29d586a13e6c9f323b87a5e9c
Isaac McGaha Miller <isaacmcgahamiller@gmail.com>
2017-04-06 13:23:10 -0600

    deploying to heroku

--- knexfile.js
+++ knexfile.js
@@ -20,5 +20,15 @@ module.exports = {
     seeds: {
       directory: __dirname + '/db/seeds'
     }
+  },
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds'
+    }
   }
 };
